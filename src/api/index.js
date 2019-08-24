492a3ad7c5e9759e59c4c380abb031bb0478e887
tom_plb <goodMorning_glb@atguigu.com>
2019-08-24 19:13:53 +0800

    37_尚硅谷_Vue项目_定义接口请求函数

--- src/api/index.js
+++ src/api/index.js
@@ -13,22 +13,22 @@ export const reqFoodCategorys = () => ajax(BASE_URL+'/index_category')
 export const reqShops = (longitude,latitude) => ajax(BASE_URL+'/shops',{longitude,latitude})
 //4、根据经纬度和关键字搜索商铺列表
 //示例http://localhost:3000/search_shops?keyword=test&geohash=40.10038,116.36867
-export const reqSearchShop = (geohash, keyword) => ajax('/search_shops','{geohash, keyword}')
-//5、获取一次性验证码
+export const reqSearchShop = (geohash, keyword) => ajax(BASE_URL+'/search_shops',{geohash, keyword})
+//5、获取一次性验证码(不用调用ajax请求)
 //示例http://localhost:3000/captcha
 export const reqCode = () => ajax('/captcha')
 //6、用户名密码登陆
 //示例http://localhost:3000/login_pwd
-export const reqPwdLogin = ({name, pwd, captcha}) => ajax('/login_pwd',{name, pwd, captcha},'POST')
+export const reqPwdLogin = ({name, pwd, captcha}) => ajax(BASE_URL+'/login_pwd',{name, pwd, captcha},'POST')
 //7、发送短信验证码
 //示例http://localhost:3000/sendcode?phone=13716962779
-export const reqSendCode = (phone) => ajax('/sendcode',{phone})
+export const reqSendCode = (phone) => ajax(BASE_URL+'/sendcode',{phone})
 //8、手机号验证码登陆
 //示例http://localhost:3000/login_sms
-export const reqSmsLogin = (phone,code) => ajax('/login_sms',{phone,code},'POST')
+export const reqSmsLogin = (phone,code) => ajax(BASE_URL+'/login_sms',{phone,code},'POST')
 //9、根据会话获取用户信息
 //示例http://localhost:3000/userinfo
-export const reqUserInfo = () => ajax('/userinfo')
+export const reqUserInfo = () => ajax(BASE_URL+'/userinfo')
 //10、用户登出
 //示例http://localhost:3000/logout
-export const reqLogout = () => ajax('/logout')
+export const reqLogout = () => ajax(BASE_URL+'/logout')
