2885fcf58694e889aed231b28e1d16ed4bd9e4c9
tom_plb <goodMorning_glb@atguigu.com>
2019-08-24 16:25:21 +0800

    34_尚硅谷_Vue项目_登陆界面效果5_前台表达验证

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -9,18 +9,19 @@
         </div>
       </div>
       <div class="login_content">
-        <form>
-         <!-- <div class="on" v-if="loginWay">-->
-          <div :class="{on:loginWay}" >
+        <form @submit.prevent="login">
+          <!-- <div class="on" v-if="loginWay">-->
+          <div :class="{on:loginWay}">
             <section class="login_message">
               <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
-              <button :disabled="!rightPhone" class="get_verification" :class = "{right_phone:rightPhone}"
-              @click.prevent="getCode">
-                {{computeTime>0?`已发送(${computeTime}s)`:"获取验证码"}}</button>
-               <!-- {{computeTime>0?computeTime+'s':"获取验证码"}}</button>-->
+              <button :disabled="!rightPhone" class="get_verification" :class="{right_phone:rightPhone}"
+                      @click.prevent="getCode">
+                {{computeTime>0?`已发送(${computeTime}s)`:"获取验证码"}}
+              </button>
+              <!-- {{computeTime>0?computeTime+'s':"获取验证码"}}</button>-->
             </section>
             <section class="login_verification">
-              <input type="tel" maxlength="8" placeholder="验证码">
+              <input type="tel" maxlength="8" placeholder="验证码" v-model="code">
             </section>
             <section class="login_hint">
               温馨提示：未注册硅谷外卖帐号的手机号，登录时将自动注册，且代表已同意
@@ -30,23 +31,23 @@
           <div :class="{on:!loginWay}">
             <section>
               <section class="login_message">
-                <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
+                <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名" v-model="name">
               </section>
               <section class="login_verification">
-                <input type="password" maxlength="8" placeholder="密码" v-if="showPwd">
-                <input type="text" maxlength="8" placeholder="密码" v-else>
+                <input type="password" maxlength="8" placeholder="密码" v-if="showPwd" v-model="pwd">
+                <input type="text" maxlength="8" placeholder="密码" v-else v-model="pwd">
                 <div class="switch_button " :class="showPwd ? 'on':'off'" @click="showPwd = !showPwd">
                   <div class="switch_circle" :class="{right:showPwd}"></div>
                   <span class="switch_text">{{showPwd ? '':'显示'}}</span>
                 </div>
               </section>
               <section class="login_message">
-                <input type="text" maxlength="11" placeholder="验证码" >
+                <input type="text" maxlength="11" placeholder="验证码" v-model="captcha">
                 <img class="get_verification" src="./images/captcha.svg" alt="captcha">
               </section>
             </section>
           </div>
-          <button class="login_submit" >登录</button>
+          <button class="login_submit">登录</button>
         </form>
         <a href="javascript:;" class="about_us">关于我们</a>
       </div>
@@ -54,41 +55,98 @@
         <i class="iconfont icon-jiantou2"></i>
       </a>
     </div>
+    <AlertTip :alertText="alertText" v-show="alertShow" @closeTip="closeTip"></AlertTip>
   </section>
 </template>
 <script>
+  import AlertTip from '../../components/AlertTip/AlertTip'
+
   export default {
-    data(){
+    data() {
       return {
         //loginWay控制短信登录和密码登录的样式.true代表短信登录,false代表密码登录
-        loginWay:true,
-        phone:'',//手机号
-        pwd:'',//密码
-        computeTime:0,//计时时间
-        showPwd:true
+        loginWay: true,
+        name: '',//用户名
+        phone: '',//手机号
+        pwd: '',//密码
+        code: '',//短信验证码
+        captcha: '',//图形验证码
+        computeTime: 0,//计时时间
+        showPwd: true,
+        alertText: '',//提示文本
+        alertShow: false//是否显示警告框
       }
     },
-    computed:{
-      rightPhone(){
+    computed: {
+      rightPhone() {
         return /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/.test(this.phone)
       }
     },
-    methods:{
-      getCode(){
+    methods: {
+      //手机验证码倒计时
+      getCode() {
         //启动定时器
-        if(!this.computeTime){
+        if (!this.computeTime) {
           this.computeTime = 10
-          const intervalId = setInterval(() =>{
+          const intervalId = setInterval(() => {
             this.computeTime--
-            if(this.computeTime <=0){
+            if (this.computeTime <= 0) {
               //停止计时
               clearInterval(intervalId)
             }
-          },1000)
+          }, 1000)
           //发送ajax请求短信验证码
         }
 
+      },
+
+      showAlert(alertText){
+        this.alertText = alertText
+        this.alertShow = true
+      },
+      
+      //前台表单验证
+      login() {
+        //首先判断是短信登录还是密码登录
+        if (this.loginWay) {//短信验证登录
+          //取出手机号和验证码
+          const {rightPhone, phone, code} = this
+          if (!rightPhone) {
+            //提示用户手机号不正确
+            this.showAlert('手机号不正确')
+            return
+          } else if (!/^\d{6}$/.test(code)) {
+            //验证码必须是6位数字
+            this.showAlert('验证码必须是6位数字')
+            return
+          }
+        } else {//密码验证登录
+          const {name, pwd, captcha} = this
+          if (!name) {
+            //提示用户名必须指定
+            this.showAlert('用户名不能为空')
+            return
+          } else if (!pwd) {
+            //密码必须指定
+            this.showAlert('密码不能为空')
+            return
+          } else if (!captcha) {
+            //图形验证码必须指定
+            this.showAlert('图形验证码不能为空')
+            return
+          }
+        }
+
+
+      },
+      //关闭提示框
+      closeTip(){
+        this.alertShow = false
+        this.alertText = ''
       }
+    },
+    components: {
+      AlertTip
     }
 
   }
@@ -100,20 +158,24 @@
     width 100%
     height 100%
     background #fff
+
     .loginInner
       padding-top 60px
       width 80%
       margin 0 auto
+
       .login_header
         .login_logo
           font-size 40px
           font-weight bold
           color #02a774
           text-align center
+
         .login_header_title
           padding-top 40px
           text-align center
-          >a
+
+          > a
             color #333
             font-size 14px
             padding-bottom 4px
@@ -123,16 +185,20 @@
               margin-left 40px
               float left
               text-decoration none
+
             &.on
               color #02a774
               font-weight 700
               border-bottom 2px solid #02a774
+
       .login_content
-        >form
-          >div
+        > form
+          > div
             display none
+
             &.on
               display block
+
             input
               width 100%
               height 100%
@@ -142,14 +208,17 @@
               border-radius 4px
               outline 0
               font 400 14px Arial
+
               &:focus
                 border 1px solid #02a774
+
             .login_message
               position relative
               margin-top 16px
               height 48px
               font-size 14px
               background #fff
+
               .get_verification
                 position absolute
                 top 50%
@@ -159,19 +228,22 @@
                 color #ccc
                 font-size 14px
                 background transparent
+
                 &.right_phone
                   color black
+
             .login_verification
               position relative
               margin-top 16px
               height 48px
               font-size 14px
               background #fff
+
               .switch_button
                 font-size 12px
                 border 1px solid #ddd
                 border-radius 8px
-                transition background-color .3s,border-color .3s
+                transition background-color .3s, border-color .3s
                 padding 0 6px
                 width 30px
                 height 16px
@@ -181,14 +253,18 @@
                 top 50%
                 right 10px
                 transform translateY(-50%)
+
                 &.off
                   background #fff
+
                   .switch_text
                     float right
                     color #ddd
+
                 &.on
                   background #02a774
-                >.switch_circle
+
+                > .switch_circle
                   //transform translateX(27px)
                   position absolute
                   top -1px
@@ -198,17 +274,21 @@
                   border 1px solid #ddd
                   border-radius 50%
                   background #fff
-                  box-shadow 0 2px 4px 0 rgba(0,0,0,.1)
+                  box-shadow 0 2px 4px 0 rgba(0, 0, 0, .1)
                   transition transform .3s
+
                   &.right
                     transform translateX(27px)
+
             .login_hint
               margin-top 12px
               color #999
               font-size 14px
               line-height 20px
-              >a
+
+              > a
                 color #02a774
+
           .login_submit
             display block
             width 100%
@@ -221,19 +301,22 @@
             font-size 16px
             line-height 42px
             border 0
+
         .about_us
           display block
           font-size 12px
           margin-top 20px
           text-align center
           color #999
+
       .go_back
         position absolute
         top 5px
         left 5px
         width 30px
         height 30px
-        >.iconfont
+
+        > .iconfont
           font-size 20px
           color #999
 </style>
